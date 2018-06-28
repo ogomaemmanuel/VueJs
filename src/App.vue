b9f2b432b934da92e034b61c45922869b4955fad
Emmanuel <ogoma.emmanuel@gmail.com>
2018-06-28 12:10:16 +0300

    added changes to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+
+        <form>
+            <input v-model="todo.title" id="todo-title-txt" type="text"></input>
+            <input v-model="todo.description" id="todo-description-txt" type="text"></input>
+            <input v-model="todo.startDate" id="todo-start-date" type="datetime-local"></input>
+            <button @click="addTodo()" type="button">Add ToDo</button>
+        </form>
+        {{msg}}
+       <span v-bind:class="{red:todos.length>10}">{{todos.length}}</span>
+        <span v-if="todos.length>10">
+            You added More than 10
+        </span>
+
+        <table>
+            <thead>
+            <tr>
+                <th>id</th>
+                <th>Create Date</th>
+                <th>Description</th>
+                <th>Start Date </th>
+                <th></th>
+            </tr>
+            </thead>
+            <tr v-for="todo in todos">
+                <td>{{todo.id}}</td>
+                <td>{{todo.createDate}}</td>
+                <td>{{todo.description}}</td>
+                <td>{{todo.startDate}}</td>
+                <button @click="deleteToDo(todo)">Delete</button>
+            </tr>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        data() {
+            return {
+                msg: "Hello",
+                todo: {},
+                todos: [{
+                    /*id: (new Date()).getTime(),
+                    createDate: new Date(),
+                    title: "Test Title",
+                    description: "Task a",
+                    startDate: new Date(),*/
+
+
+                }]
+            }
+        },
+        methods: {
+            // need annotation due to `this` in return type
+
+            addTodo() {
+                this.todo.id=(new Date()).getTime();
+                this.todo.createDate=new Date();
+                this.todos.push(this.todo);
+                this.todo={};
+            },
+            deleteToDo(todoToDelete) {
+                console.log(todoToDelete);
+                this.todos=this.todos.filter(todo => todo.id!==todoToDelete.id );
+
+            }
+        },
+
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.red{
+    color: red;
 }
+
 </style>
